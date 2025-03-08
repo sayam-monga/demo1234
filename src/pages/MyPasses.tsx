0b74578ca2abe24001509e868d3c067d641365d5
sayam-monga <71427191+sayam-monga@users.noreply.github.com>
2025-03-08 14:18:06 +0530

    Update MyPasses.tsx

--- src/pages/MyPasses.tsx
+++ src/pages/MyPasses.tsx
@@ -165,7 +165,7 @@ const MyPasses = () => {
                         <CalendarDays className="text-bollywood-red w-5 h-5 mr-2" />
                         <div>
                           <p className="text-white/60 text-xs">Date</p>
-                          <p className="text-white">March 25, 2025</p>
+                          <p className="text-white">March 13, 2025</p>
                         </div>
                       </div>
                       <div className="flex items-center">
